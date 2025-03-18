3d4b507ab1a7574ac3dd19f2f0eb6358bf7b6b22
sofonyasmaru <sofonyasmaru1@gmail.com>
2025-03-19 00:24:45 +0300

    Update isupper.py

--- Sofonyas Maru/String Method/isupper()/isupper.py
+++ Sofonyas Maru/String Method/isupper()/isupper.py
@@ -1,1 +1,10 @@
 
+def check_is_upper(string):
+    if string.isupper():
+        print(f"The string '{string}' is completely uppercase!")
+    else:
+        print(f"The string '{string}' is NOT completely uppercase.")
+
+check_is_upper("APPLE")   # Should print that the string is completely uppercase which in this case is true
+check_is_upper("Apple")   # Should print that the string is NOT completely uppercase this time its false 
+check_is_upper("125")   # Should print that the string is NOT completely uppercase this time its false
