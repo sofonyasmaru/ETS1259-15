605214d4cd4a7d27c638bfa5e39348c319fcb79a
sofonyasmaru <sofonyasmaru1@gmail.com>
2025-03-19 22:00:01 +0300

    Create islower.py
    The islower() method in Python is a string method that checks if all the characters in a string are lowercase. -It returns True if all characters are lowercase letters and there is at least one character in the string; otherwise, it returns False.

---
+++ Sofonyas Maru/String Method/islower/islower.py
@@ -0,0 +1,13 @@
+
+
+A = "apple"
+print(A.islower())  # Output: True
+
+Aa = "apple"
+print(Aa.islower())  # Output: False
+
+Aaa = "adamsapple"
+print(Aaa.islower())  # Output: True
+
+empty_string = ""
+print(empty_string.islower())  # Output: False
