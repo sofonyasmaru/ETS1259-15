24164148bd14db3b864c152beb1538a589ccd7ae
sofonyasmaru <sofonyasmaru1@gmail.com>
2025-03-23 19:58:34 +0300

    Create replace.py

---
+++ Sofonyas Maru/String Method/Str.replace()/replace.py
@@ -0,0 +1,6 @@
+text = "I love apples. apples are tasty."
+
+# Replace "apples" with "oranges"
+new_text = text.replace("apples", "oranges")
+
+print(new_text)  # Output: "I love oranges. oranges are tasty."
