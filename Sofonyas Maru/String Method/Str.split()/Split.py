22a6af62de26a519c3b1d807121bb45a0eb22f47
sofonyasmaru <sofonyasmaru1@gmail.com>
2025-03-25 20:23:20 +0300

    Create Split.py

---
+++ Sofonyas Maru/String Method/Str.split()/Split.py
@@ -0,0 +1,16 @@
+text = "apple banana orange"
+
+# Split text into a list
+words = text.split()
+
+print(words)  # Output: ['apple', 'banana', 'orange']
+
+
+or we can specify a custom delimiter
+
+text = "apple,banana,orange"
+
+# Split using comma
+words = text.split(",")
+
+print(words)  # Output: ['apple', 'banana', 'orange']
