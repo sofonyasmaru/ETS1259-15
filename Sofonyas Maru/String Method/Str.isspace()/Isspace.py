6de3ee9501a520dc873fdba7bd3c08d2d35e1c9b
sofonyasmaru <sofonyasmaru1@gmail.com>
2025-03-27 21:04:29 +0300

    Create Isspace.py

---
+++ Sofonyas Maru/String Method/Str.isspace()/Isspace.py
@@ -0,0 +1,10 @@
+text = "   "  # Spaces only
+
+# Check if text contains only spaces
+result = text.isspace()
+
+print(result)  # Output: True
+
+
+text = "apple "
+print(text.isspace())  # Output: False
