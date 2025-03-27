af089556860a1510ede880fe44dbf64e643486f8
sofonyasmaru <sofonyasmaru1@gmail.com>
2025-03-27 21:08:27 +0300

    Create Format.py

---
+++ Sofonyas Maru/String Method/Str.isspace()/Str.format ()/Format.py
@@ -0,0 +1,7 @@
+name = "Alice"
+age = 25
+
+# Format a string with placeholders
+sentence = "My name is {} and I am {} years old.".format(name, age)
+
+print(sentence)  # Output: "My name is Alice and I am 25 years old."
