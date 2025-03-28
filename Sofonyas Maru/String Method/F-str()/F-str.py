c096b93dc1301f5e804ff1f6a3b0bfde4477b05c
sofonyasmaru <sofonyasmaru1@gmail.com>
2025-03-28 19:04:10 +0300

    Create F-str.py

---
+++ Sofonyas Maru/String Method/F-str()/F-str.py
@@ -0,0 +1,15 @@
+name = "Alice"
+age = 25
+
+# Using f-string
+sentence = f"My name is {name} and I am {age} years old."
+
+print(sentence)  # Output: "My name is Alice and I am 25 years old."
+
+
+or
+
+
+x = 5
+y = 10
+print(f"The sum of {x} and {y} is {x + y}.")  # Output: "The sum of 5 and 10 is 15."
