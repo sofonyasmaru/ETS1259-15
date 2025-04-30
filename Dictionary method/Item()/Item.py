aa858c7728a83fe1a3355eb768fa2536f56abbf2
sofonyasmaru <sofonyasmaru1@gmail.com>
2025-04-30 20:10:09 +0300

    Create Item.py

---
+++ Dictionary method/Item()/Item.py
@@ -0,0 +1,9 @@
+fruit_info = { "name": "apple","color": "red","price": 3.5,"quantity": 15}
+#Print all key-value pairs
+for key, value in fruit_info.items():
+    print(f"{key} -> {value}")
+  #output 
+name -> apple
+color -> red
+price -> 3.5
+quantity -> 15
